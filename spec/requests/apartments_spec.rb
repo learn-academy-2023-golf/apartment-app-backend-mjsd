e81942d3e8f5a8792040f36e1df52fb7202d7cb4
Derek Maly <dmaly18@gmail.com>
2023-12-12 15:18:07 -0800

    made update to create-functionality

--- spec/requests/apartments_spec.rb
+++ spec/requests/apartments_spec.rb
@@ -30,39 +30,61 @@ RSpec.describe "Apartments", type: :request do
         end
     end
 
-    describe "POST /create" do 
-    it 'should create apartment' do
-        apartment_params = {
+     describe "POST /create" do 
+        it 'should create apartment' do
+            apartment_params = {
             apartment: {
-                  street: "Auto Mall Parkway",  
-                  unit: "113",
-                  city: "D'Iberville",
-                  state: "MS",
-                  square_footage: 1200,
-                  price: "1300",
-                  bedrooms: 2,
-                  bathrooms: 2,
-                  pets: "cats and dogs",
-                  image: "https://media.apts247.info/ce/ce9560e9239c4c3b8369663b0a699279/floorplans/1196_CoconutPalm_504586.jpg",
-                  user_id: user.id
-            }
+            street: "Auto Mall Parkway",  
+            unit: "113",
+            city: "D'Iberville",
+            state: "MS",
+            square_footage: 1200,
+            price: "1300",
+            bedrooms: 2,
+            bathrooms: 2,
+            pets: "cats and dogs",
+            image: "https://media.apts247.info/ce/ce9560e9239c4c3b8369663b0a699279/floorplans/1196_CoconutPalm_504586.jpg",
+            user_id: user.id
         }
+    }
 
-      post '/apartments', params: apartment_params
-      expect(response).to have_http_status(200)
+            post '/apartments', params: apartment_params
+            expect(response).to have_http_status(200)
 
-      apartment = Apartment.first
+            apartment = Apartment.first
 
-      expect(apartment.street).to eq "Auto Mall Parkway"  
-      expect(apartment.unit).to eq "113"
-      expect(apartment.city).to eq "D'Iberville"
-      expect(apartment.state).to eq "MS"
-      expect(apartment.square_footage).to eq 1200
-      expect(apartment.price).to eq "1300"
-      expect(apartment.bedrooms).to eq 2
-      expect(apartment.bathrooms).to eq 2
-      expect(apartment.pets).to eq "cats and dogs"
-      expect(apartment.image).to eq "https://media.apts247.info/ce/ce9560e9239c4c3b8369663b0a699279/floorplans/1196_CoconutPalm_504586.jpg"
+            expect(apartment.street).to eq "Auto Mall Parkway"  
+            expect(apartment.unit).to eq "113"
+            expect(apartment.city).to eq "D'Iberville"
+            expect(apartment.state).to eq "MS"
+            expect(apartment.square_footage).to eq 1200
+            expect(apartment.price).to eq "1300"
+            expect(apartment.bedrooms).to eq 2
+            expect(apartment.bathrooms).to eq 2
+            expect(apartment.pets).to eq "cats and dogs"
+            expect(apartment.image).to eq "https://media.apts247.info/ce/ce9560e9239c4c3b8369663b0a699279/floorplans/1196_CoconutPalm_504586.jpg"
+        end
+
+        it "cannot create an apartment" do 
+            apartment_params = {
+            apartment: {
+            unit: "113",
+            city: "D'Iberville",
+            state: "MS",
+            square_footage: 1200,
+            price: "1300",
+            bedrooms: 2,
+            bathrooms: 2,
+            pets: "cats and dogs",
+            image: "https://media.apts247.info/ce/ce9560e9239c4c3b8369663b0a699279/floorplans/1196_CoconutPalm_504586.jpg",
+            user_id: user.id
+          }
+    }
+    
+            post '/apartments', params: apartment_params
+            apartment = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(apartment['street']).to include "can't be blank"
+        end
     end
-  end
-  end
+ end
