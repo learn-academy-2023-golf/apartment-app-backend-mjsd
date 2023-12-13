0651d19c39d2e8ea5c53ea4ac24bf2144b4cef33
Sean <swbriski@gmail.com>
2023-12-13 10:12:07 -0800

    added 422 specs

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -16,7 +16,11 @@ class ApartmentsController < ApplicationController
     def update
       apartment = Apartment.find(params[:id])
       apartment.update(apartment_params)
-      render json: apartment
+      if apartment.valid?
+        render json: apartment 
+      else
+        render json: apartment.errors, status: 422
+      end
     end
 
   private
